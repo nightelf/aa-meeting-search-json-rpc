4a8880e8154aaf8768c9168445311074ee3a9efa
jared Kuemmerle <jared.kuemmerle@yahoo.com>
2018-02-02 23:17:45 -0800

    Do command line arg parsing. capitalize Day in email. Remove Swiftmailer.

--- find_and_sort_meetings.php
+++ find_and_sort_meetings.php
@@ -7,6 +7,28 @@ use Meeting\AttendeeParser;
 use Meeting\MeetingClient;
 use Meeting\AttendeeMailer;
 
+// Parse args. Could not find a good arg parser package
+$argMap = [
+    1 => [ 'error' => 'Argument 1, the path of the attendee csv, is required' ],
+    2 => [ 'error' => 'Argument 2 the search city, is required' ],
+    3 => [ 'error' => 'Argument 3 the search state code (eg. \'CA\'), is required' ],
+];
+
+foreach ($argMap as $key => $arg) {
+    if (!isset($argv[$key])) {
+        exit("\n" . $arg['error'] . "\n");
+    }
+}
+
+if (count($argv) > 4) {
+    exit("\n" . "Too many arguments. Don't forget to double quote args with spaces." . "\n");
+}
+
+$attendeeCsvPath = $argv[1];
+$searchCity = $argv[2];
+$searchState = $argv[3];
+
+// do dotenv
 $dotenv = new Dotenv(__DIR__);
 $dotenv->load();
 $user = getenv('MEETINGS_API_USER');
@@ -15,18 +37,18 @@ $templatePath = __DIR__ . '/views/templates/';
 $templateCachePath = $templatePath . 'cache/';
 
 if (empty($user)) {
-    exit("Must define MEETINGS_API_USER in .env file");
+    exit("\n" ."Must define MEETINGS_API_USER in .env file" . "\n");
 } else if (empty($user)) {
-    exit("Must define MEETINGS_API_PASSWORD in .env file");
+    exit("\n" ."Must define MEETINGS_API_PASSWORD in .env file" . "\n");
 }
 
-
+// parse the attendees
 $attendeeParser = AttendeeParser::build();
-$attendeeCollection = $attendeeParser->parseCsvFile($argv[1]);
+$attendeeCollection = $attendeeParser->parseCsvFile($attendeeCsvPath);
 
-// Create the client
+// Create the client, get results and mail
 $meetingClient = MeetingClient::build($user, $pass);
-$meetingCollection = $meetingClient->search('San Diego', 'CA');
+$meetingCollection = $meetingClient->search($searchCity, $searchState);
 $attendeeMailer = AttendeeMailer::build($templatePath, [
     'cache' => $templateCachePath,
 ]);
